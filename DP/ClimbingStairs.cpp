3efb8b5579f034f7929178f2dad101d2bf5c30ca
Scute <prabhmeetsinghdev@gmail.com>
2024-07-26 01:17:39 +0530

    Create ClimbingStairs.cpp

---
+++ DP/ClimbingStairs.cpp
@@ -0,0 +1,24 @@
+// basic nth fibonacci number code 
+
+int solve(int n , vector<int> &dp){
+        if(n < 0){
+            return 0;
+        }
+        if(n == 0)return 0;
+        if(n == 1)return 1;
+        if(n == 2)return 2;
+        
+
+
+        if(dp[n] != -1){
+            return dp[n];
+        }
+
+        dp[n] = solve(n-1 , dp) + solve(n-2, dp);
+        return dp[n];
+    }
+    
+    int climbStairs(int n) {
+        vector<int>dp(n+1 , -1);
+        return solve(n , dp);
+    }
